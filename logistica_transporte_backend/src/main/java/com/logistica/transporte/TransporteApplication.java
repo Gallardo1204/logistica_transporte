053743155529ee9084ee06b1d14f19bbad9d7b2d
gallardo <magb1204@gmail.com>
2023-05-22 23:56:04 -0500

    Implementacion jwt

--- logistica_transporte_backend/src/main/java/com/logistica/transporte/TransporteApplication.java
+++ logistica_transporte_backend/src/main/java/com/logistica/transporte/TransporteApplication.java
@@ -1,13 +1,24 @@
 package com.logistica.transporte;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class TransporteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TransporteApplication.class, args);
-	}
+    @Value("${validation.password.pattern}")
+    private String passwordRegexp;
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransporteApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setProperty() {
+        System.setProperty("validation.password.pattern", passwordRegexp);
+    }
 
 }
